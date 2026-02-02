4ffc2ddc5549617888c3e313afcb543c29c94501
dDez21 <davidemario21@gmail.com>
2026-02-02 23:08:07 +0100

    Add custom CSS in public/CSS and include in layouts

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -13,6 +13,12 @@
 
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
+        <link rel="stylesheet" href="{{ asset('CSS/footer.css') }}">
+        <link rel="stylesheet" href="{{ asset('CSS/header.css') }}">
+        <link rel="stylesheet" href="{{ asset('CSS/guest.css') }}">
+        <link rel="stylesheet" href="{{ asset('CSS/footer.css') }}">
+        <link rel="stylesheet" href="{{ asset('CSS/components/button.css') }}">
+        <link rel="stylesheet" href="{{ asset('CSS/components/form.css') }}">
     </head>
     <body class="font-sans antialiased">
         <div class="min-h-screen bg-gray-100">
