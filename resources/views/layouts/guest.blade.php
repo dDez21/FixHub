5435969ebb4031c9ee65aa82a0378a6152daf18b
dDez21 <davidemario21@gmail.com>
2026-02-02 23:17:53 +0100

    nome sito

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -7,7 +7,7 @@
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
         
-        <title>{{ config('app.name', 'FixHub') }}</title>
+        <title>{{ config('FixHub') }}</title>
 
         <!-- stili testi -->
         <link rel="preconnect" href="https://fonts.bunny.net">
