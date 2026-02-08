8b08f69642d1c8ad20d4a1f5a87b7407202cdd42
dDez21 <davidemario21@gmail.com>
2026-02-08 22:24:26 +0100

    nuovo utente

--- resources/views/pages/admin/user/deleteUser.blade.php
+++ resources/views/pages/admin/user/deleteUser.blade.php
@@ -8,7 +8,7 @@
 <div class="delete-user-layout">
 
     <div class="delete-user-card card">
-        <h1 class="text deleteText">Sei sicuro di voler eliminare l'utente {{ $user->name }} {{ $user->surname }}?</h1>
+        <h1 class="title deleteText">Sei sicuro di voler eliminare l'utente {{ $user->name }} {{ $user->surname }}?</h1>
         
         <div class="buttons">
             <form action="{{ route('admin.users.delete', $user) }}" method="POST">
@@ -16,9 +16,8 @@
                 @method('DELETE')
                 <button type="submit" class="button">Elimina</button>
             </form>
-            
             <div class="form-confirm">
-                <button type="submit" class="button button-back">Annulla</button>
+                <button type="submit" class="button button-back" href="{{ route('admin.users') }}">Annulla</button>
             </div>
         </div>
     </div>
