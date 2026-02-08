4b425c5676546cf3f9fab1d90cf018e3a80b0be4
dDez21 <davidemario21@gmail.com>
2026-02-08 22:26:18 +0100

    nuovo utente

--- resources/views/pages/admin/user/deleteUser.blade.php
+++ resources/views/pages/admin/user/deleteUser.blade.php
@@ -10,15 +10,10 @@
     <div class="delete-user-card card">
         <h1 class="title deleteText">Sei sicuro di voler eliminare l'utente {{ $user->name }} {{ $user->surname }}?</h1>
         
-        <div class="buttons">
             <form action="{{ route('admin.users.delete', $user) }}" method="POST">
                 @csrf
                 @method('DELETE')
                 <button type="submit" class="button">Elimina</button>
             </form>
-            <div class="form-confirm">
-                <button type="submit" class="button button-back" href="{{ route('admin.users') }}">Annulla</button>
-            </div>
-        </div>
     </div>
 @endsection
