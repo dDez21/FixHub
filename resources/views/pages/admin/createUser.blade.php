707668005ad2deb310ac0a48a851980313c37f53
dDez21 <davidemario21@gmail.com>
2026-02-08 18:53:05 +0100

    nuovo utente

--- resources/views/pages/admin/createUser.blade.php
+++ resources/views/pages/admin/createUser.blade.php
@@ -13,81 +13,85 @@
     <form method="POST" action="{{ route('admin.users.store') }}" class="create-user-form">
         @csrf
 
-        <!-- nome -->
-        <div class="form-space">
-            <label class="form-label" for="name">Nome</label>
-            <input class="form-input" type="text" id="name" name="name" required>
-        </div>
-
-        <!-- cognome -->
-        <div class="form-space">
-            <label class="form-label" for="surname">Cognome</label>
-            <input class="form-input"type="text" id="surname" name="surname" required>
-        </div>
-
-        <!-- username -->
-        <div class="form-space">
-            <label class="form-label" for="username">Username</label>
-            <input class="form-input"type="text" id="username" name="username" required>
-        </div>
-
-        <!-- password -->
-        <div class="form-space">
-            <label class="form-label" for="password">Password</label>
-            <input class="form-input" type="password" id="password" name="password" required>
-        </div>
-
-        <!-- ruolo -->
-        <select class="form-space" name="role" id="role" required>
-            <label class="form-label" for="role">Ruolo</label>
-            <option class="list-value" value="tech"  @selected(old('role')=='tech')>Tecnico</option>
-            <option class="list-value" value="staff" @selected(old('role')=='staff')>Staff</option>
-            <option class="list-value" value="admin" @selected(old('role')=='admin')>Admin</option>
-        </select>
-
-
-        <!-- opzioni tecnico -->
-        <div id="tech-options" @if(!$isTech) hidden @endif>
-
-            <!-- data di nascita -->
+        <!-- elementi form -->
+        <div class="user-grid">
+            <!-- nome -->
             <div class="form-space">
-                <label class="form-label" for="birth_date">Data di nascita</label>
-                <input class="form-input"type="date" id="birth_date" name="birth_date" value="{{ old('birth_date') }}" max="{{ now()->toDateString() }}">
+                <label class="form-label" for="name">Nome</label>
+                <input class="form-input" type="text" id="name" name="name" required>
             </div>
 
-
-            <!-- centro associato -->
+            <!-- cognome -->
             <div class="form-space">
-                <label class="form-label" for="center">Centro</label>
-                <select name="center_id" id="center_id" class="list-space">                    
-                    <option value="">Nessun centro</option>
-                    @foreach($centers as $center)
-                        <option class="list-value" value="{{ $center->id }}" @selected(old('center_id')==$center->id)>
-                        {{ $center->name }}, {{ $center->city }}
-                        </option>
-                    @endforeach                    
+                <label class="form-label" for="surname">Cognome</label>
+                <input class="form-input"type="text" id="surname" name="surname" required>
+            </div>
+
+            <!-- username -->
+            <div class="form-space">
+                <label class="form-label" for="username">Username</label>
+                <input class="form-input"type="text" id="username" name="username" required>
+            </div>
+
+            <!-- password -->
+            <div class="form-space">
+                <label class="form-label" for="password">Password</label>
+                <input class="form-input" type="password" id="password" name="password" required>
+            </div>
+
+            <!-- ruolo -->
+            <div class="form-space">    
+                <label class="form-label" for="role">Ruolo</label>
+                <select class="form-space" name="role" id="role" required>
+                    <option class="list-value" value="tech"  @selected(old('role')=='tech')>Tecnico</option>
+                    <option class="list-value" value="staff" @selected(old('role')=='staff')>Staff</option>
+                    <option class="list-value" value="admin" @selected(old('role')=='admin')>Admin</option>
                 </select>
             </div>
 
+            <!-- opzioni tecnico -->
+            <div id="tech-options" @if(!$isTech) hidden @endif>
 
-                <!-- categorie -->
-                <div class="form-group">
+                <!-- data di nascita -->
+                <div class="form-space">
+                    <label class="form-label" for="birth_date">Data di nascita</label>
+                    <input class="form-input"type="date" id="birth_date" name="birth_date" value="{{ old('birth_date') }}" max="{{ now()->toDateString() }}">
+                </div>
 
-                    <p class="text form-label">Categorie</p>
 
-                    <div class="categories-box">
-                        @foreach($categories as $category)
-                        <label class="category-item">
-                            <input type="checkbox"
-                                name="categories[]"
-                                value="{{ $category->id }}"
-                                @checked(in_array($category->id, old('categories', [])))>
-                            <span>{{ $category->name }}</span>
-                        </label>
-                        @endforeach
-                    </div>
-            </div>   
-        </div>
+                <!-- centro associato -->
+                <div class="form-space">
+                    <label class="form-label" for="center">Centro</label>
+                    <select name="center_id" id="center_id" class="list-space">                    
+                        <option value="">Nessun centro</option>
+                        @foreach($centers as $center)
+                            <option class="list-value" value="{{ $center->id }}" @selected(old('center_id')==$center->id)>
+                            {{ $center->name }}, {{ $center->city }}
+                            </option>
+                        @endforeach                    
+                    </select>
+                </div>
+
+
+                    <!-- categorie -->
+                    <div class="form-group">
+
+                        <p class="text form-label">Categorie</p>
+
+                        <div class="categories-box">
+                            @foreach($categories as $category)
+                            <label class="category-item">
+                                <input type="checkbox"
+                                    name="categories[]"
+                                    value="{{ $category->id }}"
+                                    @checked(in_array($category->id, old('categories', [])))>
+                                <span>{{ $category->name }}</span>
+                            </label>
+                            @endforeach
+                        </div>
+                </div>   
+            </div>
+        </div>    
     
         <!-- conferma creazione -->
         <div class="form-confirm">
