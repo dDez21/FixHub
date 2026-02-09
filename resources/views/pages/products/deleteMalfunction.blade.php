24218c8d8446935b253beb0594b1b6a623137227
dDez21 <davidemario21@gmail.com>
2026-02-09 18:17:20 +0100

    malfunzionamenti

--- resources/views/pages/products/deleteMalfunction.blade.php
+++ resources/views/pages/products/deleteMalfunction.blade.php
@@ -10,7 +10,7 @@
     <div class="delete-user-card card">
         <h1 class="title deleteText">Sei sicuro di voler eliminare il malfunzionamento?</h1>
         
-            <form  action="{{ route('staff.products.malfunctions.update', [$product, $malf]) }}"  method="POST">
+            <form  action="{{ route('staff.products.malfunctions.delete', [$product, $malfunctions]) }}"  method="POST">
                 @csrf
                 @method('DELETE')
                 <button type="submit" class="button">Elimina</button>
