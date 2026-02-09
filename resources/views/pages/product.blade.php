5f6effd972fc1a9645dbc0c397a01ca9c8b1f85d
dDez21 <davidemario21@gmail.com>
2026-02-09 16:58:36 +0100

    malfunzionamenti

--- resources/views/pages/product.blade.php
+++ resources/views/pages/product.blade.php
@@ -18,18 +18,6 @@
         <p class="medium-text product-cat">Guida all'installazione</p>
         <p class="product-data">{{ $product->installation }}</p>
 
-        <!-- malfunzionamenti -->
-        @if(auth()->check() && (auth()->user()->role === 'tech' || auth()->user()->role === 'staff'))
-            @php
-                $routeName = auth()->user()->role === 'staff'
-                    ? 'staff.products.malfunctions'
-                    : 'tecn.products.malfunctions';
-            @endphp
-
-            <a id="product-edit-link" class="add-user" href="{{ route($routeName, $product) }}">
-                <p class="medium-text product-cat">Elenco malfunzionamenti -></p>
-            </a>
-        @endif
 
         <!-- modifica o rimuovi prodotto -->
         @if(auth()->check() && (auth()->user()->role === 'tech' || auth()->user()->role === 'staff'))
@@ -41,7 +29,7 @@
 
             <p class="medium-text product-cat">Malfunzionamenti</p>
 
-            <a class="product-data product-link-row" href="{{ route($routeName, $product) }}">
+            <a class="product-data" href="{{ route($routeName, $product) }}">
                 Vedi elenco malfunzionamenti →
             </a>
         @endif
