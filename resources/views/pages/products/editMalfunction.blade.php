af141e9ed1f6b797647fd4fd56d12ef418359a28
dDez21 <davidemario21@gmail.com>
2026-02-09 18:16:33 +0100

    malfunzionamenti

--- resources/views/pages/products/editMalfunction.blade.php
+++ resources/views/pages/products/editMalfunction.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 
 <div class="card malfunction-space">
-  <form method="POST" action="{{ route('staff.products.malfunctions.update', $malf) }}" class="create-malfunction-form">
+  <form method="POST" action="{{ route('staff.products.malfunctions.update', [$product, $malf]) }}"  class="create-malfunction-form">
     @csrf
     @method('PUT')
 
