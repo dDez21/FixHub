18a0838e1ed6a3207025e293d65ec2b9abe4ee43
dDez21 <davidemario21@gmail.com>
2026-03-23 18:02:43 +0100

    sistemazioni

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -109,8 +109,8 @@
 
 
        <!-- livelli di accesso --> 
-        <div class="service-card">
-            <h1 class="medium-text">Livelli di accesso</h1>
+        <div class="access-level-card">
+            <h1 class="medium-text-access">Livelli di accesso</h1>
 
             <p class="small-text">
                 <strong>Tecnico</strong>: visualizzazione delle informazioni del sito e dell’azienda, dell’elenco dei centri di assistenza, del catalogo prodotti e delle relative schede, inclusi malfunzionamenti e soluzioni, senza possibilità di modifica.<br><br>
