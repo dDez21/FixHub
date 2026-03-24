f5872580a2713f951ca7f67afc4b93539c48b653
dDez21 <davidemario21@gmail.com>
2026-03-24 18:34:34 +0100

    sistemazioni

--- resources/views/pages/admin/centers/createCenter.blade.php
+++ resources/views/pages/admin/centers/createCenter.blade.php
@@ -21,7 +21,7 @@
         <div class="button-section">
             <!-- annulla creazione -->
             <div class="form-confirm">
-                <button type="button" class="button button-back" onclick="history.back()">Annulla</button>
+                <button type="button" class="button button-back" onclick="window.location='{{ route('where') }}'">Annulla</button>
             </div>
             
             <!-- conferma creazione -->
--- resources/views/pages/admin/user/createUser.blade.php
+++ resources/views/pages/admin/user/createUser.blade.php
@@ -23,7 +23,7 @@
         <div class="button-section">
             <!-- annulla creazione -->
             <div class="form-confirm">
-                <button type="button" class="button button-back" onclick="window.location='{{ route('admin.users.show') }}'">Annulla</button>
+                <button type="button" class="button button-back" onclick="window.location='{{ route('admin.users') }}'">Annulla</button>
             </div>
             
             <!-- conferma creazione -->
