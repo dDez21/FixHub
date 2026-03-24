b40fbf20a4b056e0e67750dcbd3299d928c43ab5
dDez21 <davidemario21@gmail.com>
2026-03-24 14:48:55 +0100

    sistemazioni

--- resources/views/pages/admin/centers/deleteCenter.blade.php
+++ resources/views/pages/admin/centers/deleteCenter.blade.php
@@ -10,14 +10,16 @@
     <div class="delete-user-card card">
         <h1 class="title deleteText">Sei sicuro di voler eliminare il centro {{ $center->name }}?</h1>
 
-            <div class="form-confirm">
-                <button type="button" class="button button-back" onclick="history.back()">Annulla</button>
-            </div>
+            <div class="button-section">
+                <div class="form-confirm">
+                    <button type="button" class="button button-back" onclick="history.back()">Annulla</button>
+                </div>
 
-            <form action="{{ route('admin.centers.delete', $center) }}" method="POST">
-                @csrf
-                @method('DELETE')
-                <button type="submit" class="button">Elimina</button>
-            </form>
+                <form action="{{ route('admin.centers.delete', $center) }}" method="POST">
+                    @csrf
+                    @method('DELETE')
+                    <button type="submit" class="button">Elimina</button>
+                </form>
+            </div>
     </div>
 @endsection
